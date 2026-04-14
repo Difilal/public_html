db0f639ec548ef94f9539506f72254ee227720c2
filal <filal@pmpland.co.id>
2026-04-14 15:53:07 +0700

    Add initial index.php file for PHP application

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+date_default_timezone_set('Asia/Jakarta');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP App</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+
+<h1>🚀 Selamat Datang di Aplikasi PHP</h1>
+<p>Deploy otomatis menggunakan Jenkins CI/CD</p>
+
+<p>Waktu server: <?php echo date("Y-m-d H:i:s"); ?></p>
+
+<hr>
+
+<a href="about.php">Tentang</a> |
+<a href="contact.php">Kontak</a>
+
+</body>
+</html>
